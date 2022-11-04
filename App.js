f931e9d5392a01f3806ba784ea3cd6429d7ed2af
Joyin2 <joyinlaskar2000@gmail.com>
2022-11-04 21:09:45 +0530

    package added

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar
+        backgroundColor={"white"}
+        barStyle={"dark-content"}
+        translucent={false}
+      />
+      <Text>
+        App Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
+        modi placeat corporis esse fugit nisi minima delectus tempore quam, rem
+        enim consectetur suscipit!hhv
+      </Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
+export default App;
